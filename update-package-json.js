4f6ac18d112d5e377ea946783badc129b2f43f2f
Cyber Pool <86299091+cyberpoolorg@users.noreply.github.com>
2021-12-21 10:29:54 +0100

    Add files via upload

--- update-package-json.js
+++ update-package-json.js
@@ -4,15 +4,15 @@ const uiPackageFilePath = 'package.json';
 const uiPackageFile = fs.readFileSync(uiPackageFilePath, 'utf8');
 const uiPackage = JSON.parse(uiPackageFile);
 
-const corePackageFilePath = './qortal-ui-core/package.json';
+const corePackageFilePath = '../qortal-ui-core/package.json';
 const corePackageFile = fs.readFileSync(corePackageFilePath, 'utf8');
 const corePackage = JSON.parse(corePackageFile);
 
-const cryptoPackageFilePath = './qortal-ui-crypto/package.json';
+const cryptoPackageFilePath = '../qortal-ui-crypto/package.json';
 const cryptoPackageFile = fs.readFileSync(cryptoPackageFilePath, 'utf8');
 const cryptoPackage = JSON.parse(cryptoPackageFile);
 
-const pluginsPackageFilePath = './qortal-ui-plugins/package.json';
+const pluginsPackageFilePath = '../qortal-ui-plugins/package.json';
 const pluginsPackageFile = fs.readFileSync(pluginsPackageFilePath, 'utf8');
 const pluginsPackage = JSON.parse(pluginsPackageFile);
 
